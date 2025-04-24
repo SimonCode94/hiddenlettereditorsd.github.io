63f46e3048c180831014266ceee492c586a70a4c
simon <liaoxiying94@gmail.com>
2025-04-24 19:08:14 +0800

    编辑器提交

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Dyeing-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/2e5f43ee86c709777053d4b47a70ca90.framework.js",
-    "Build/c15f1f1a86e34bfc0d79f9a85d7a9015.data",
-    "Build/9848f00caf64a116bec759aa08f42b15.wasm",
+    "Build/0bd40d5eb96c4da9647b0926f0e9b85c.framework.js",
+    "Build/bb43f595d6d0bf49c1419f7e8be9109e.data",
+    "Build/e96b762abee990447da70e2211340500.wasm",
     "TemplateData/style.css"
 
 ];
