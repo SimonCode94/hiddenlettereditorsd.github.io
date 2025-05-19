27cee5c4921b0aba5964e274f59e536fe7373431
simon <liaoxiying94@gmail.com>
2025-05-19 10:20:14 +0800

    打包

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Dyeing-1.0";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/6b446a81fdcba292089045aa029eeded.framework.js",
-    "Build/abc58b13bf97e92100a3661bdf82f069.data",
-    "Build/58745c9afc09847bc35421a3a8de9525.wasm",
+    "Build/6781683bc96f6da4c1c0656abfe7fd9e.framework.js",
+    "Build/a748d05e8fd92edde0e708518cd95025.data",
+    "Build/99251c74219f64ffe95e77ef4d1ec4e5.wasm",
     "TemplateData/style.css"
 
 ];
